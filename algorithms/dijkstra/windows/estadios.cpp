a87a4e60883337c871d45d82bbca49283747e90d
Leo Andrade <od.leonardo01@gmail.com>
2020-08-19 03:32:35 -0300

    updating files

---
+++ algorithms/dijkstra/windows/estadios.cpp
@@ -0,0 +1,255 @@
+//#include <cstdlib>
+#include <iostream>
+#include <string>
+
+#include "desc_estadios.h"
+#include "estadios.h"
+#include "initializer.h"
+#include "prog_utils.h"
+
+/**	--------------------------------------
+	FUNÇÃO PRIVADA PARA RETORNAR ESTRUTURA
+	--------------------------------------
+
+	@param: desc_club dc --> Struct para ser retornada
+ **/
+desc_club Estadios::getCopy (desc_club dc) {
+
+	desc_club DC;
+	DC.bairro = dc.bairro;
+	DC.cep = DC.cep;
+	DC.cidade_uf = dc.cidade_uf;
+	DC.clube = dc.clube;
+	DC.cod = dc.cod;
+	DC.endereco = dc.endereco;
+	DC.estadio = dc.estadio;
+	DC.pais = dc.pais;
+
+	return DC;
+
+}
+
+// Construtor
+Estadios::Estadios () {
+	init_club(DESC_CLUB);
+}
+
+/**	-----------------
+	GETTERS E SETTERS
+	-----------------
+ **/
+
+
+desc_club Estadios::getDesc_club (int index) {
+	return getCopy (DESC_CLUB[index]);
+}
+
+
+void Estadios::setCodigo (int index, int cod) {
+	DESC_CLUB[index].cod = cod;
+}
+
+
+int Estadios::getCodigo (int index) {
+	return DESC_CLUB[index].cod;
+}
+
+
+void Estadios::setClube (int index, std::string clube) {
+	DESC_CLUB[index].clube = clube;
+}
+
+
+std::string Estadios::getClube (int index) {
+	return DESC_CLUB[index].clube.data();
+}
+
+
+void Estadios::setEstadio (int index, std::string estadio) {
+	DESC_CLUB[index].estadio = estadio;
+}
+
+
+std::string Estadios::getEstadio (int index) {
+	return DESC_CLUB[index].estadio.data();
+}
+
+
+void Estadios::setPais (int index, std::string pais) {
+	DESC_CLUB[index].pais = pais;
+}
+
+
+std::string Estadios::getPais (int index) {
+	return DESC_CLUB[index].pais.data();
+}
+
+
+void Estadios::setCidadeUF (int index, std::string cidade_uf) {
+	DESC_CLUB[index].cidade_uf = cidade_uf;
+}
+
+
+std::string Estadios::getCidadeUF (int index) {
+	return DESC_CLUB[index].cidade_uf.data();
+}
+
+
+void Estadios::setBairro (int index, std::string bairro) {
+	DESC_CLUB[index].bairro = bairro;
+}
+
+
+std::string Estadios::getBairro (int index) {
+	return DESC_CLUB[index].bairro.data();
+}
+
+
+void Estadios::setEndereco (int index, std::string endereco) {
+	DESC_CLUB[index].endereco = endereco;
+}
+
+
+std::string Estadios::getEndereco (int index) {
+	return DESC_CLUB[index].endereco.data();
+}
+
+
+void Estadios::setCep (int index, std::string cep) {
+	DESC_CLUB[index].cep = cep;
+}
+
+
+std::string Estadios::getCep (int index) {
+	return DESC_CLUB[index].cep.data();
+}
+
+
+/**	--------------------
+	FUNÇÕES DE IMPRESSÃO
+	--------------------
+ **/
+
+
+/**	---------------------------------------
+	FUNÇÃO PARA MOSTRAR UM CLUBE ESPECÍFICO
+	---------------------------------------
+
+	@param: int index --> Posição do clube na struct
+ **/
+void Estadios::showClube (int index) {
+	std::cout << DESC_CLUB[index].clube.data();
+}
+
+
+/**	-------------------------------------------------------
+	FUNÇÃO PARA MOSTRAR TODOS OS CÓDIGOS E NOMES DOS CLUBES
+	-------------------------------------------------------
+
+	@param: NoNe
+ **/
+void Estadios::showCodigos () {
+
+	for (int index = 0; index < CLUBES; index++) {
+		print_block_line(1, 0, PRINT_BLOCK, '=', true, true);
+		std::cout << " " << DESC_CLUB[index].clube << std::endl;
+		print_block_line(1, 0, PRINT_BLOCK, '=', true, true);
+
+		std::cout << "\t" << "CÓDIGO: " << DESC_CLUB[index].cod << std::endl;
+
+		print_block_line(1, 0, PRINT_BLOCK, '-', true, true);
+		std::cout << std::endl << std::endl;
+
+	}
+
+}
+
+
+/**	-----------------------------------------
+	FUNÇÃO PARA MOSTRAR UM ESTÁDIO ESPECÍFICO
+	-----------------------------------------
+
+	@param: int index --> Posição do estádio na struct
+ **/
+void Estadios::showEstadio (int index) {
+
+	print_block_line(1, 0, PRINT_BLOCK, '=', true, true);
+	std::cout << " CLUBE " << DESC_CLUB[index].cod << " - " << DESC_CLUB[index].clube.data() << std::endl;
+	print_block_line(1, 0, PRINT_BLOCK, '=', true, true);
+
+	std::cout << "\t" << "ESTÁDIO: " << DESC_CLUB[index].estadio.data() << std::endl;
+
+	print_block_line(1, 0, PRINT_BLOCK, '-', true, true);
+	std::cout << std::endl << std::endl;
+
+}
+
+
+/**	-------------------------------------
+	FUNÇÃO PARA MOSTRAR TODOS OS ESTÁDIOS
+	-------------------------------------
+
+	@param: NoNe
+ **/
+void Estadios::showEstadios () {
+	for (int index = 0; index < CLUBES; index++) {
+
+		print_block_line(1, 0, PRINT_BLOCK, '=', true, true);
+		std::cout << " CLUBE " << DESC_CLUB[index].cod << " - " << DESC_CLUB[index].clube.data() << std::endl;
+		print_block_line(1, 0, PRINT_BLOCK, '=', true, true);
+
+		std::cout << "\t" << "ESTÁDIO: " << DESC_CLUB[index].estadio.data() << std::endl;
+
+		print_block_line(1, 0, PRINT_BLOCK, '-', true, true);
+		std::cout << std::endl << std::endl;
+
+	}
+}
+
+
+/**	-------------------------------------------
+	FUNÇÃO PARA MOSTRAR INFORMAÇÕES DE UM CLUBE
+	-------------------------------------------
+
+	@param: int index --> Posição do clube na struct
+ **/
+void Estadios::showClubInfo (int index) {
+
+	print_block_line (1, 0, PRINT_BLOCK, '=', true, true);
+	std::cout << " CLUBE " << DESC_CLUB[index].cod << " -- " << DESC_CLUB[index].clube.data() << " (INFO)" << std::endl;
+	print_block_line (1, 0, PRINT_BLOCK, '=', true, true);
+
+	std::cout << "\t" << "ESTÁDIO    : " << DESC_CLUB[index].estadio.data() << std::endl;
+	std::cout << "\t" << "PAÍS       : " << DESC_CLUB[index].pais.data() << std::endl;
+	std::cout << "\t" << "CIDADE (UF): " << DESC_CLUB[index].cidade_uf.data() << std::endl;
+	std::cout << "\t" << "BAIRRO     : " << DESC_CLUB[index].bairro.data() << std::endl;
+	std::cout << "\t" << "ENDEREÇO   : " << DESC_CLUB[index].endereco.data() << std::endl;
+	std::cout << "\t" << "CEP        : " << DESC_CLUB[index].cep.data() << std::endl;
+
+	std::cout << std::endl << std::endl;
+}
+
+
+/**	--------------------------------------------------
+	FUNÇÃO PARA MOSTRAR INFORMAÇÕES DE TODOS OS CLUBES
+	--------------------------------------------------
+
+	@param: NoNe
+ **/
+void Estadios::showClubsInfo () {
+
+	for (int index = 0; index < CLUBES; index++) {
+		print_block_line (1, 0, PRINT_BLOCK, '=', true, true);
+		std::cout << " CLUBE " << DESC_CLUB[index].cod << " -- " << DESC_CLUB[index].clube.data() << " (INFO)" << std::endl;
+		print_block_line (1, 0, PRINT_BLOCK, '=', true, true);
+
+		std::cout << "\t" << "ESTÁDIO    : " << DESC_CLUB[index].estadio.data() << std::endl;
+		std::cout << "\t" << "PAÍS       : " << DESC_CLUB[index].pais.data() << std::endl;
+		std::cout << "\t" << "CIDADE (UF): " << DESC_CLUB[index].cidade_uf.data() << std::endl;
+		std::cout << "\t" << "BAIRRO     : " << DESC_CLUB[index].bairro.data() << std::endl;
+		std::cout << "\t" << "ENDEREÇO   : " << DESC_CLUB[index].endereco.data() << std::endl;
+		std::cout << "\t" << "CEP        : " << DESC_CLUB[index].cep.data() << std::endl;
+
+		std::cout << std::endl << std::endl;
+	}
+}
--- algorithms/dijkstra/windows/grafo.h
+++ algorithms/dijkstra/windows/grafo.h
@@ -16,264 +16,113 @@
 class Grafo {
 	private:
 		int nvertices;	// Número de vértices do grafo
-		
+
 		std::list <std::pair <int, int> > * adj;	// variável responsável por realizar a adjacência entre 2 nós
 
 	public:
-		
+
 		// Construtor
-		Grafo (int nvertices) {
-			
-			this->nvertices = nvertices;
-			adj = new std::list <std::pair <int, int> >[nvertices];
-			
-		}
-	
-		
+		Grafo (int nvertices);
+
+
 		/**	---------------------------------------------------
 			MÉTODO PARA CRIAR UMA ARESTA DIRECIONAL ENTRE 2 NÓS
 			---------------------------------------------------
-			
+
 			@param: int v1 --> Nó de origem da aresta
-			
+
 			@param: int v2 --> Nó de destino da aresta
-			
+
 			@param: int custo --> custo da aresta
 		 **/
-		void addAresta (int v1, int v2, int custo) {
-			
-			adj[v1].push_back (std::make_pair (v2, custo));
-			
-		}
-		
-		
+		void addAresta (int v1, int v2, int custo);
+
+
 		/**	-------------------------------------
 			MÉTODO PARA IMPRIMIR O VALOR DE UM NÓ
 			-------------------------------------
-			
+
 			@param: stdist **arv --> arvore com dijkstra implementado
-			
+
 			@param: int pos --> posição da árvore a ser impressa
-			
+
 			@param: int b --> quantidade de vezes que a tabulação será realizada
-			
+
 			@param: int *type_print --> tipo de impressão (0 --> Imprime o código do nó,
 														   1 --> Imprime o nome do clube;
 														   2 --> Imprime o nome do estádio;
 		 **/
-		void imprimeNo (stdist **arv, int pos, int b, int *type_print) {
-			
-			int i;
-			
-			std::cout << std::endl;
-			
-			for (i = 0; i < b; i++) std::cout << ((*type_print)  ? "\t\t" : "\t");	// Imprime identação
-			
-			// Verifica o tipo de impressão
-			switch ((*type_print)) {
-				
-				case 0:	// Código do clube
-					std::cout << "Nó " << arv[pos]->cod;
-					break;
-				
-				case 1:	// Nome do clube
-					std::cout << handlerClube(pos);
-					break;
-				
-				case 2:	// Nome do estádio
-					std::cout << handlerEstadio(pos);
-					break;
-				
-				default:
-					return;
-					break;
-				
-			}
-			
-			std::cout << " (";
-			if ((*type_print)) std::cout << "Nó " << arv[pos]->cod << ", "; // Se o tipo de impressão for 1 ou 2, imprime o código do nó como adicional
-			std::cout << "custo = " << arv[pos]->custo << ")";	// Impressão do custo do nó
-			
-		}
-		
-		
+		void imprimeNo (stdist **arv, int pos, int b, int *type_print);
+
+
 		/**	--------------------------------------------
 			MÉTODO PARA MOSTRAR A ÁRVORE GERADORA MÍNIMA
 			--------------------------------------------
-			
+
 			@param: stdist **arv --> arvore com dijkstra implementado
-			
+
 			@param: int pos --> posição da árvore a ser impressa
-			
+
 			@param: int b --> quantidade de vezes que a tabulação será realizada
-			
+
 			@param: stdist *size --> tamanho do vetor de ligação entre as árvores
-			
+
 			@param: int *type_print --> tipo de impressão (0 --> Imprime o código do nó,
 														   1 --> Imprime o nome do clube;
 														   2 --> Imprime o nome do estádio;
-														   
+
 			@param: int passo_a_passo --> indica se o usuário deseja printar a árvore de uma vez, ou passo a passo
 										  (false --> Imprime toda a árvore seu pausa
 										   true  --> Imprime a árvore com pausas
 		 **/
-		void showGrafo (stdist **a, int pos, int b, int *size, int *type_print, bool passo_a_passo) {
-			
-			int next = 0;
-			
-			imprimeNo (a, pos, b, type_print);
-			
-			for (int i = 0; i < (*size); i++)
-				// Verifica se há conexão posterior
-				if (a[pos]->next[i]) {
-					
-					if (!(next)) {
-						std::cout << " --> ";
-						std::cout << std::endl;
-					}
-					
-					if (passo_a_passo) while (getch() != 13);
-					
-					showGrafo (a, i, b + 1, size, type_print, passo_a_passo);	// Entra na conexão posterior
-					std::cout << std::endl;
-					
-					next++;
-					
-				}
-			
-			if (!(next)) return;	// Se não ouve nenhuma conexão, retorna a atividade anterior
-			
-		}
-		
-		
+		void showGrafo (stdist **a, int pos, int b, int *size, int *type_print, bool passo_a_passo);
+
+
 		/**	---------------------------------------------
 			FUNÇÃO PARA DESCOBRIR O TAMANHO DE UM CAMINHO
 			---------------------------------------------
-			
+
 			@param: stdist **a --> Árvore armazenando o caminho
-			
+
 			@param: int orig --> Posição de origem do caminho
-			
+
 			@param: int dest --> Posição de destino do caminho
 		 **/
-		int getPathLen (stdist **a, int orig, int dest) {
-			int i = 1;
-			
-			if (orig == dest) {
-				
-				return 1;
-				
-			}
-			
-			return i + getPathLen(a, orig, a[dest]->previous->cod - 1);
-		}
-		
-		
+		int getPathLen (stdist **a, int orig, int dest);
+
+
 		/**	------------------------------------
 			MÉTODO PARA MOSTRAR O CAMINHO MÍNIMO
 			------------------------------------
-			
+
 			@param: stdist **arv --> arvore com dijkstra implementado
-			
+
 			@param: int orig --> posição da origem do caminho
-			
+
 			@param: int dest --> posição da destino do caminho
-			
+
 			@param: int b --> quantidade de vezes que a tabulação será realizada
-			
+
 			@param: int *type_print --> tipo de impressão (0 --> Imprime o código do nó,
 														   1 --> Imprime o nome do clube;
 														   2 --> Imprime o nome do estádio;
-														   
+
 			@param: int passo_a_passo --> indica se o usuário deseja printar a árvore de uma vez, ou passo a passo
 										  (false --> Imprime toda a árvore seu pausa
 										   true  --> Imprime a árvore com pausas
 		 **/
-		void showPath (stdist **a, int orig, int dest, int b, int *type_print, bool passo_a_passo) {
-			
-			
-			if (dest == orig) {
-				imprimeNo (a, a[orig]->cod - 1, b, type_print);
-				
-				return;
-			}
-			
-			showPath(a, orig, a[dest]->previous->cod - 1, b - 1, type_print, passo_a_passo);	// Entra em recursão até que chegue na origem
-			std::cout << " --> ";
-			if (passo_a_passo) while (getch() != 13);
-			std::cout << std::endl;
-			imprimeNo (a, a[dest]->cod - 1, b, type_print);
-			
-		}
-		
-		
+		void showPath (stdist **a, int orig, int dest, int b, int *type_print, bool passo_a_passo);
+
+
 		/**	---------------------------------------------
 			MÉTODO DE IMPLEMENTAÇÃO DO ALGORITMO DIJKSTRA
 			---------------------------------------------
-			
+
 			@param: int orig --> posição de origem do grafo
-			
+
 			@param: stdist **arvore --> grafo
 		 **/
-		void dijkstra (int orig, stdist **arvore) {
-			
-			// fechados conta quantos nós foram fechados
-			// dist[] guarda os pesos dos vértices
-			int fechados = 0, dist[nvertices];
-			int visitados[nvertices];
-			
-			// Variável para gerenciar fila de prioridades
-			std::priority_queue <std::pair <int, int>, std::vector <std::pair <int, int> >, std::greater <std::pair <int, int> > > pq;
-			
-			// Inicia os pesos dos vértices ("INFINITO")
-			for (int i = 0; i < nvertices; i++) {
-				dist[i] = INT_MAX / 2;
-				visitados[i] = false;
-			}
-			
-			dist[orig] = 0;	// Peso do vértice de origem passa a ser 0
-			
-			pq.push (std::make_pair (dist[orig], orig));	// Adiciona o nó de origem na fila de prioridades
-			
-			while (!pq.empty()) {
-				
-				std::pair <int, int> p = pq.top();
-				int u = p.second;
-				pq.pop();
-				
-				if (visitados[u] == false) {
-					
-					visitados[u] = true;
-					
-					// Iterador it para percorrer as adjacências de um vértice
-					std::list <std::pair <int, int> >::iterator it;
-					
-					for (it = adj[u].begin(); it != adj[u].end(); it++) {
-						
-						int v = it->first;	// Código do nó
-						int custo_aresta = it->second;	// Custo da adjacência
-						
-						// Se o custo for da iteração atual for menor que o armazenado,
-						// substitui o custo e o caminho até o nó e o adiciona na fila de prioridades
-						if (dist[v] > (dist[u] + custo_aresta)) {
-							
-							dist[v] = dist[u] + custo_aresta;
-							pq.push (std::make_pair (dist[v], v));
-							
-							if (arvore[v]->previous != NULL) arvore[v]->previous->next[v] = 0;
-							arvore[u]->next[v] = dist[v];
-							arvore[v]->previous = arvore[u];
-							
-						}
-					}
-				}
-			}
-			
-			// Atualiza os pesos de cada vértice na struct
-			for (int i = 0; i < CLUBES; i++)
-				arvore[i]->custo = dist[i];
-			
-		}
+		void dijkstra (int orig, stdist **arvore);
 };
 
 
