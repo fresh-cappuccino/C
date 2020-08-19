bd089c528a467ad2220f7085de4232ccd5a6b1da
Leo Andrade <od.leonardo01@gmail.com>
2020-08-19 16:47:01 -0300

    updating files

---
+++ conditional_structures/operators/unary/unary_operator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main ()
+{
+	int a = 13;
+	int b = -21;
+
+	std::cout << "A  = " << a << std::endl;
+	std::cout << "-A = " << -a << std::endl;
+	std::cout << "B  = " << b << std::endl;
+	std::cout << "-B = " << -b << std::endl;
+}
